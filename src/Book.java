c810237d1d74d064a82967ef19fbb522a13ea497
Bhavani Putrevu <Home@Venkats-MacBook-Air.local>
2018-01-22 23:01:34 -0600

    Added Java Classes

---
+++ src/Book.java
@@ -0,0 +1,77 @@
+import com.sun.tools.javac.util.List;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * Created by Home on 1/22/18.
+ */
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private LocalDate dueDate;
+    private User borrowdBy;
+    private List<Book> holds;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public User getBorrowdBy() {
+        return borrowdBy;
+    }
+
+    public void setBorrowdBy(User borrowdBy) {
+        this.borrowdBy = borrowdBy;
+    }
+
+    public List<Book> getHolds() {
+        return holds;
+    }
+
+    public void setHolds(List<Book> holds) {
+        this.holds = holds;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", dueDate=" + dueDate +
+                ", borrowdBy=" + borrowdBy +
+                ", holds=" + holds +
+                '}';
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,63 @@
+import java.util.List;
+
+/**
+ * Created by Home on 1/22/18.
+ */
+public class User {
+    private String id;
+    private String name;
+    private String address;
+    private String phone;
+    private List<Book> booksOnHold;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Book> getBooksOnHold() {
+        return booksOnHold;
+    }
+
+    public void setBooksOnHold(List<Book> booksOnHold) {
+        this.booksOnHold = booksOnHold;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", booksOnHold=" + booksOnHold +
+                '}';
+    }
+}
